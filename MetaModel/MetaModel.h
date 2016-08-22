63e6fc8e3a168416e5026ec9da5e1a9421994a99
Draveness <stark.draven@gmail.com>
2016-08-22 11:55:45 +0800

    Initialize

---
+++ MetaModel/MetaModel.h
@@ -0,0 +1,19 @@
+//
+//  MetaModel.h
+//  MetaModel
+//
+//  Created by 左书祺 on 8/22/16.
+//  Copyright © 2016 metamodel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MetaModel.
+FOUNDATION_EXPORT double MetaModelVersionNumber;
+
+//! Project version string for MetaModel.
+FOUNDATION_EXPORT const unsigned char MetaModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MetaModel/PublicHeader.h>
+
+

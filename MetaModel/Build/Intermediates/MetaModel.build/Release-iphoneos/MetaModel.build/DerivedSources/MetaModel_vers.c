f4d019fff65df16d0e61aed18aee08ac36dd0555
Draveness <stark.draven@gmail.com>
2016-09-07 11:14:18 +0800

    add gitignore

--- MetaModel/Build/Intermediates/MetaModel.build/Release-iphoneos/MetaModel.build/DerivedSources/MetaModel_vers.c
+++
@@ -1,1 +0,0 @@
- const unsigned char MetaModelVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:MetaModel  PROJECT:MetaModel-1" "\n"; const double MetaModelVersionNumber __attribute__ ((used)) = (double)1.;

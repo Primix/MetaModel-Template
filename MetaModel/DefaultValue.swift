fc0cdac11d1c0734762ade2d3ec1fd65c23c9265
Draveness <stark.draven@gmail.com>
2016-09-10 15:00:17 +0800

    Add default value swift file MModel/MetaModel#22

---
+++ MetaModel/DefaultValue.swift
@@ -0,0 +1,14 @@
+//
+//  DefaultValue.swift
+//  MetaModel
+//
+//  Created by Draveness on 9/10/16.
+//  Copyright © 2016 metamodel. All rights reserved.
+//
+
+import Foundation
+
+let IntDefaultValue = Int(INT64_MAX)
+let StringDefaultValue = "MModel.MetaModel.DefaultValue"
+let DoubleDefaultValue = Double.infinity
+let NSDateDefaultValue = NSDate(timeIntervalSince1970: 0)

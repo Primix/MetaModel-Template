8c6aa55ed3240051f463f48bc28af25e7531143e
Draveness <stark.draven@gmail.com>
2016-09-15 20:16:58 +0800

    Add Transaction to SQLBridge swift file

--- MetaModel/SQLBridge.swift
+++ MetaModel/SQLBridge.swift
@@ -28,6 +28,23 @@ func executeSQL(sql: String, verbose: Bool = false, suppress: Bool = false, succ
     return nil
 }
 
+func executeTransaction(sqls: [String], verbose: Bool = false, block: Void -> Void) -> [Statement] {
+    var result: [Statement] = []
+    let startDate = NSDate()
+    do {
+        try db.transaction {
+            for sql in sqls {
+                result.append(try db.run(sql))
+            }
+        }
+    } catch let error {
+        let endDate = NSDate()
+        let interval = endDate.timeIntervalSinceDate(startDate) * 1000
+        print("SQL: ROLLBACK | (\(interval.format("0.2"))ms) \(sqls) | ERROR: \(error)")
+    }
+    return result
+}
+
 func executeScalarSQL(sql: String, verbose: Bool = false, success: (() -> ())? = nil) -> Binding? {
     let startDate = NSDate()
     let result = db.scalar(sql)

882d3a0498132554318a9b98fdb60fe9dc2c7250
Draveness <stark.draven@gmail.com>
2016-09-15 13:52:49 +0800

    Update suppress support to SQLiteBrighe

--- MetaModel/SQLBridge.swift
+++ MetaModel/SQLBridge.swift
@@ -9,55 +9,31 @@
 import Foundation
 
 func executeSQL(sql: String, verbose: Bool = false, suppress: Bool = false, success: (() -> ())? = nil) -> Statement? {
-    if verbose {
-        print("-> Begin Transaction")
-    }
     let startDate = NSDate()
     do {
         let result = try db.run(sql)
         let endDate = NSDate()
         let interval = endDate.timeIntervalSinceDate(startDate) * 1000
 
-        if verbose {
-            print("\tSQL (\(interval.format("0.2"))ms) \(sql)")
-            print("-> Commit Transaction")
-            print("\n")
-        }
-        if let success = success {
-            success()
-        }
+        if verbose { print("SQL: SUCCEED  | (\(interval.format("0.2"))ms) \(sql)") }
+
+        if let success = success { success() }
 
         return result
     } catch let error {
         let endDate = NSDate()
         let interval = endDate.timeIntervalSinceDate(startDate) * 1000
-        print("\tSQL (\(interval.format("0.2"))ms) \(sql)")
-        print("\t\(error)")
-        if verbose {
-            print("-> Rollback transaction")
-            print("\n")
-        }
+        if !suppress { print("SQL: ROLLBACK | (\(interval.format("0.2"))ms) \(sql) | ERROR: \(error)") }
     }
     return nil
 }
 
-func executeScalarSQL(sql: String, verbose: Bool = false, suppress: Bool = false, success: (() -> ())? = nil) -> Binding? {
-    if verbose {
-        print("-> Begin Transaction")
-    }
+func executeScalarSQL(sql: String, verbose: Bool = false, success: (() -> ())? = nil) -> Binding? {
     let startDate = NSDate()
     let result = db.scalar(sql)
     let endDate = NSDate()
     let interval = endDate.timeIntervalSinceDate(startDate) * 1000
-    if verbose {
-        print("\tSQL (\(interval.format("0.2"))ms) \(sql)")
-        print("-> Commit Transaction")
-        print("\n")
-    }
-
-    if let success = success {
-        success()
-    }
-
+    if verbose { print("SQL: SUCCEED  | (\(interval.format("0.2"))ms) \(sql)") }
+    if let success = success { success() }
     return result
 }
